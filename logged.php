7fc25db02e5a697938ee8240a58783972539e145
Jared-Adamson <jared.d.adamson@gmail.com>
2014-10-30 01:41:00 -0700

    Added Part 2

--- logged.php
+++ logged.php
@@ -1,1 +1,33 @@
+<!--
+//Jared Adamson
+//CPI310
+//Lab 14
+//Part 2
+-->
 
+
+<?php
+
+session_start();  
+
+echo "Logged In";
+
+echo"<br><br>";
+
+echo "Session ID: "; 
+
+$s_id = session_id();
+
+echo $s_id;
+
+echo "<br><br>";
+
+echo "Created For: ";
+
+echo $_SESSION[userName];
+
+echo "<br><br>";
+
+session_destroy(); 
+
+?>
--- users.php
+++ users.php
@@ -1,1 +1,56 @@
+<!--
+//Jared Adamson
+//CPI310
+//Lab 14
+//Part 2
+-->
+
+
+<?php
+ 
+  session_start();  // start the session
+
+  $user = $_POST['user'];
+ 
+  $password = $_POST['password'];
+ 
+  $registeredUsers = array("User" => "User", "Password" => "Password");
+
+  session_name('$user');  // set the session name
+
+  session_save_path();
+
+  $_SESSION['userName']=$user;
+
+
+if($user == $registeredUsers['User']){
+
+	if($password == $registeredUsers['Password']){
+
+	echo "Login Successful you will be redirected to the main page in 5 seconds.";
+ 	header("refresh:5; url=logged.php");
+
+	}else{
+
+	echo "Invalid Password";
+
+	session_destroy(); 
+
+	}
+
+
+
+}else{
+
+echo "Invalid User Name";
+
+session_destroy(); 
+
+}
+
+
+?>
+
+
+
 
